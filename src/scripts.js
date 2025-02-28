218aad355a076351adc1064e3fd47d255789dfa9
aurelienbono <bonombelleaurelien08@gmail.com>
2025-02-28 20:21:14 +0000

    add new operations

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,12 @@
 var number = 12 
 var sommes = 0 
 var secondNumber = 13 
+var thirdNumber = 23 
 
 
 var result = number + secondNumber ; 
+var newResult = result = thirdNumber ; 
 
 
 console.log(result)
+console.log(newResult)
